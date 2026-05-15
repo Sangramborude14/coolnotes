5c95dd39681332cf276afce2c14e6273244ed3b5
Sangram <sangram2mail@gmail.com>
2026-05-15 13:27:56 +0530

    database connected

---
+++ app/api/notes/route.ts
@@ -0,0 +1,33 @@
+import { connectToDatabase } from "@/lib/db";
+import {Note} from "@/lib/models/Note"
+import { NextResponse } from "next/server";
+import {auth} from "@/lib/auth";
+import {headers} from "next/headers";
+
+export async function POST(req: Request){
+    try{
+        await connectToDatabase();
+
+        const session = await auth.api.getSession({
+            headers:await
+            headers()
+        })
+
+        if(!session) {
+            return NextResponse.json({error: "Unauthorized"},{status: 401})
+        }
+        const body = await req.json();
+        const {heading,content} = body;
+     
+        const newNote = await Note.create({
+            username: session.user.name,
+            noteId: Date.now() + heading.replace(/\s+/g,'-'),
+            heading,
+            content,
+            isPrivate: true
+        })
+        return NextResponse.json(newNote,{status: 201});
+}catch(error){
+    console.error("Error saving the Note")
+    return NextResponse.json({error: "Failed to save note"},{status: 500});
+}}
--- app/notes/create/page.tsx
+++ app/notes/create/page.tsx
@@ -1,4 +1,20 @@
+"use client"
+
+import { useState } from "react"
+
 export default function CreateNote(){
+    const [title,setTitle] = useState("");
+    const [content,setContent] = useState("");
+
+    const handleSave = async (e: React.FormEvent) => {
+        e.preventDefault();
+        const res = await fetch("/api/notes",{
+            method: "POST",
+            body: JSON.stringify({heading:title,content:content})
+        })
+        if (res.ok) alert("saved to database")
+    }
+
     return(
         <>
        <div
@@ -8,15 +24,15 @@ export default function CreateNote(){
             </h1>
         <div>
             <form>
-                <input type="text" id="heading" name="heading" placeholder="Heading"
+                <input type="text" id="heading" name="heading" placeholder="Heading" onChange={(e) => setTitle(e.target.value)}
                  className="border-none bg-transparent text-5xl font-bold
                             focus:outline-none text-center w-full mb-8"/>
                 
-                <textarea placeholder = "WRITE YOUR NOTES HERE 📝" rows={15} className="bg-transparent border-none focus:outline-none text-xl w-full max-w-6xl px-4 text-center">
+                <textarea placeholder = "WRITE YOUR NOTES HERE 📝" rows={15} className="bg-transparent border-none focus:outline-none text-xl w-full max-w-6xl px-4 text-center" onChange={(e) => setContent(e.target.value)}>
                 </textarea>
                 
                 <div>
-                    <button type="submit" className="border-2 border-white  hover:bg-primary mt-4 p-2 hover:scale-105 transition-all duration-200 transform ">SAVE</button>
+                    <button type="submit" className="border-2 border-white  hover:bg-primary mt-4 p-2 hover:scale-105 transition-all duration-200 transform " onClick={handleSave}>SAVE</button>
                 </div>
             </form>
         </div>
