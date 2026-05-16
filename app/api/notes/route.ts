9a382240be0188e42895c1e327113cec9fe50a97
Sangram <sangram2mail@gmail.com>
2026-05-16 06:33:40 +0530

    added searchbar

--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -1,62 +1,63 @@
 import { connectToDatabase } from "@/lib/db";
-import {Note} from "@/lib/models/Note"
+import { Note } from "@/lib/models/Note"
 import { NextResponse } from "next/server";
-import {auth} from "@/lib/auth";
-import {headers} from "next/headers";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 
-export async function POST(req: Request){
-    try{
+export async function POST(req: Request) {
+    try {
         await connectToDatabase();
 
         const session = await auth.api.getSession({
-            headers:await
-            headers()
+            headers: await
+                headers()
         })
 
-        if(!session) {
-            return NextResponse.json({error: "Unauthorized"},{status: 401})
+        if (!session) {
+            return NextResponse.json({ error: "Unauthorized" }, { status: 401 })
         }
         const body = await req.json();
-        const {heading,content} = body;
-     
+        const { heading, content } = body;
+
         const newNote = await Note.create({
             username: session.user.name,
-            noteId: Date.now() + heading.replace(/\s+/g,'-'),
+            noteId: Date.now() + heading.replace(/\s+/g, '-'),
             heading,
             content,
             private: true
         })
-        return NextResponse.json(newNote,{status: 201});
-}catch(error){
-    console.error("Error saving the Note")
-    return NextResponse.json({error: "Failed to save note"},{status: 500});
-}}
+        return NextResponse.json(newNote, { status: 201 });
+    } catch (error) {
+        console.error("Error saving the Note")
+        return NextResponse.json({ error: "Failed to save note" }, { status: 500 });
+    }
+}
 
 export async function GET(req: Request) {
-    try{
+    try {
         await connectToDatabase();
 
         const session = await auth.api.getSession({
             headers: await headers()
         })
-        if(!session){
-            return NextResponse.json({error:"unauthorized"},{status: 401});
+        if (!session) {
+            return NextResponse.json({ error: "unauthorized" }, { status: 401 });
         }
-        const notes = await Note.find({username: session.user.name})
+        const notes = await Note.find({ username: session.user.name })
         return NextResponse.json(notes)
-    }catch(error){
-        return NextResponse.json({error: "Failed to connect to database"},{status: 500});
+    } catch (error) {
+        return NextResponse.json({ error: "Failed to connect to database" }, { status: 500 });
     }
 }
 
 export async function DELETE(req: Request) {
-    try{
+    try {
         await connectToDatabase();
-        const {noteId} = await req.json();
-        
-        await Note.findOneAndDelete({noteId});
-        return NextResponse.json({message: "deleted successfully"});
-    }catch(error){
-        return NextResponse.json({error: "Delete UNsuccessful"},{status: 500});
+        const { noteId } = await req.json();
+
+        await Note.findOneAndDelete({ noteId });
+        return NextResponse.json({ message: "deleted successfully" });
+    } catch (error) {
+        return NextResponse.json({ error: "Delete UNsuccessful" }, { status: 500 });
     }
 }
--- app/notes/create/page.tsx
+++ app/notes/create/page.tsx
@@ -2,55 +2,55 @@
 
 import { useState } from "react"
 
-export default function CreateNote(){
-    const [title,setTitle] = useState("");
-    const [content,setContent] = useState("");
+export default function CreateNote() {
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
 
-   const handleSave = async (e: React.FormEvent) => {
-    e.preventDefault(); // Prevents page reload
-    
-    try {
-        const res = await fetch("/api/notes", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ heading: title, content: content })
-        });
+    const handleSave = async (e: React.FormEvent) => {
+        e.preventDefault(); // Prevents page reload
 
-        const result = await res.json();
+        try {
+            const res = await fetch("/api/notes", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ heading: title, content: content })
+            });
 
-        if (res.ok) {
-            alert("Saved successfully! 🚀");
-        } else {
-            alert(`Error: ${result.error || "Failed to save"}`);
+            const result = await res.json();
+
+            if (res.ok) {
+                alert("Saved successfully! 🚀");
+            } else {
+                alert(`Error: ${result.error || "Failed to save"}`);
+            }
+        } catch (error) {
+            alert("Network error: Could not reach the server.");
         }
-    } catch (error) {
-        alert("Network error: Could not reach the server.");
-    }
-};
+    };
 
 
-    return(
+    return (
         <>
-       <div
-       className="flex flex-col items-center justify-start min-h-screen pt-20">
-         <h1 className="text-sm uppercase tracking-widest text-gray-400 text-center pb-3">
-            Create the coolest note possible
-            </h1>
-        <div>
-            <form onSubmit={handleSave}>
-                <input type="text" id="heading" name="heading" placeholder="Heading" onChange={(e) => setTitle(e.target.value)}
-                 className="border-none bg-transparent text-5xl font-bold
-                            focus:outline-none text-center w-full mb-8"/>
-                
-                <textarea placeholder = "WRITE YOUR NOTES HERE 📝" rows={15} className="bg-transparent border-none focus:outline-none text-xl w-full max-w-6xl px-4 text-center" onChange={(e) => setContent(e.target.value)}>
-                </textarea>
-                
+            <div
+                className="flex flex-col items-center justify-start min-h-screen pt-20">
+                <h1 className="text-sm uppercase tracking-widest text-gray-400 text-center pb-3">
+                    Create the coolest note possible
+                </h1>
                 <div>
-                    <button type="submit" className="border-2 border-white  hover:bg-primary mt-4 p-2 hover:scale-105 transition-all duration-200 transform ">SAVE</button>
+                    <form onSubmit={handleSave}>
+                        <input type="text" id="heading" name="heading" placeholder="Heading" onChange={(e) => setTitle(e.target.value)}
+                            className="border-none bg-transparent text-5xl font-bold
+                            focus:outline-none text-center w-full mb-8"/>
+
+                        <textarea placeholder="WRITE YOUR NOTES HERE 📝" rows={15} className="bg-transparent border-none focus:outline-none text-xl w-full max-w-6xl px-4 text-center" onChange={(e) => setContent(e.target.value)}>
+                        </textarea>
+
+                        <div>
+                            <button type="submit" className="border-2 border-white  hover:bg-primary mt-4 p-2 hover:scale-105 transition-all duration-200 transform ">SAVE</button>
+                        </div>
+                    </form>
                 </div>
-            </form>
-        </div>
-       </div>
+            </div>
         </>
     )
 }
